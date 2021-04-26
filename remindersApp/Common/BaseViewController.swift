c3c4cb0f7724911ed8cc16107fec1e37016d42ac
Povilas Staskus <povilas.staskus@gmail.com>
2021-04-26 19:06:08 +0300

    Added keyboard appearing / disappearing logic to BaseViewController

--- remindersApp/Common/BaseViewController.swift
+++ remindersApp/Common/BaseViewController.swift
@@ -12,4 +12,37 @@ class BaseViewController: UIViewController {
     
     func setupView() {}
     func setupConstraints() {}
+    
+    func keyboardWillAppear(_ keyboardHeight: CGFloat) {}
+    func keyboardWillDisappear() {}
+}
+
+extension BaseViewController {
+    func observeKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(notifaction: NSNotification) {
+        guard let keyboardFrame = (notifaction.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        self.keyboardWillAppear(keyboardFrame.height)
+    }
+    
+    @objc private func keyboardWillHide() {
+        keyboardWillDisappear()
+    }
 }
